9b85479b19da611e7eab7b025e38c4fd5ce71989
Ben <benjamin_weaver@hotmail.com>
2023-09-11 13:36:14 +0100

    added project directories

---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+module Main (main) where
+
+import Control.Concurrent.STM
+import Control.Concurrent
+import Control.Monad -- hiding (forever)
+
+
+import HDT.Tasks
+import HDT.Interpreters hiding (ping, pong) 
+
+
+
+-- THIS ROUTINE COMPILES, BUT DOES NOT WORK. 
+
+-- FOR DEMONSTRACTION OF THREADS, 
+   -- run MyPingPong.hs with this commands:
+      -- stack ghci     -- in directory containing MyPingPong.hs
+      -- main
+        
+main :: IO ()
+main = runIO [ping, pong]
+
+
+
+
